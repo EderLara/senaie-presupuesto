697b529a7f3aa1b9b54525ac647e21bb748b4dd7
Eder Lara T <ederlatru@gmail.com>
2023-05-13 10:30:34 -0500

    Conf static y login

--- core/views.py
+++ core/views.py
@@ -5,3 +5,14 @@ from django.views.generic import TemplateView
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = 'core/index.html'
+
+    # atributos propios en diccionarios de contexto:
+    dict_context = {
+        'promo' : ['Prepara', 'Dispone', 'Gestiona'],
+        'framework' : 'DJANGO',
+        'titulo_mapa': 'Encuentranos aquí:'
+    }
+
+    # Sobreescribimos el método get de la clase TemplateView:
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, self.dict_context)
--- src/settings.py
+++ src/settings.py
@@ -123,7 +123,21 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Configuración de arhivos media y staticos:
+MEDIA_URL = '/assets/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/assets/img/')
+
+# indicamos donde se encuentran los archivos static:
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Mis configuraciones:
+LOGIN_REDIRECT_URL = 'admin'
